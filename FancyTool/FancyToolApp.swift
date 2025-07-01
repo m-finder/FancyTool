b550a3270ceea168855a59ab1ae7cba395004e2c
吴雲放 <m@m-finder.com>
2025-07-01 15:31:47 +0800

    Initial Commit

---
+++ FancyTool/FancyToolApp.swift
@@ -0,0 +1,32 @@
+//
+//  FancyToolApp.swift
+//  FancyTool
+//
+//  Created by 吴雲放 on 2025/7/1.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FancyToolApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
