5964e04e0822a6d5cf290e9ab898822ab907bfa1
吴雲放 <m@m-finder.com>
2025-07-14 17:18:02 +0800

    update

---
+++ FancyTool/Modules/Texter/TexterPopoverView.swift
@@ -0,0 +1,115 @@
+//
+//  TexterPopoverView.swift
+//  FancyTool
+//
+//  Created by 吴雲放 on 2025/7/1.
+//
+
+import SwiftUI
+
+struct TexterPopoverView: View {
+  
+  private var colorUtil: ColorUtil = ColorUtil()
+  @ObservedObject var state = AppState.shared
+  
+  var body: some View {
+    VStack(alignment: .center) {
+      
+      Image("default")
+        .resizable()
+        .aspectRatio(
+          contentMode: .fit
+        ).frame(
+          width: 60,
+          height: 60
+        ).padding(.top, 15)
+      
+      // 标题
+      if state.colorIndex == 0{
+        Text("Texter").font(.system(
+          size: 24,
+          weight: .semibold,
+          design: .rounded
+        ))
+      }else{
+        Text("Texter").font(.system(
+          size: 24,
+          weight: .semibold,
+          design: .rounded
+        ))
+        .foregroundStyle(LinearGradient(
+          colors: colorUtil.getColor(
+            index: state.colorIndex
+          ),
+          startPoint: .topLeading,
+          endPoint: .bottomTrailing
+        ))
+      }
+      
+      
+      // 菜单栏文本
+      VStack(alignment: .leading) {
+        
+        Divider()
+        
+        LabelledDivider(
+          label: String(
+            localized: "MenuBar Text"
+          )
+        )
+        
+        ZStack {
+          
+          TextField(
+            String(localized: "Input something"),
+            text: state.$text
+          ).onChange(of: state.text) {
+            if state.text.count > 15 {
+              state.text = String(state.text.prefix(15))
+            }
+          }.textFieldStyle(.roundedBorder)
+          
+        }
+      }
+      
+      // 多彩文字
+      VStack{
+        
+        LabelledDivider(label: String(localized: "Colorful Text"))
+        
+        LazyVGrid(
+          columns: Array(
+            repeating: GridItem(
+              .flexible(minimum: 20),
+              spacing: 10
+            ),
+            count: 10
+          ),
+          spacing: 10
+        ) {
+          
+          ForEach(0..<colorUtil.getColors().count, id: \.self) { index in
+            Button {
+              state.colorIndex = index
+            } label: {
+              Image(systemName: state.colorIndex == index ? "checkmark.square.fill" : "square.fill")
+                .foregroundStyle(LinearGradient(
+                  colors: colorUtil.getColor(index: index),
+                  startPoint: .topLeading,
+                  endPoint: .bottomTrailing
+                ))
+            }.buttonStyle(.plain)
+              .frame(
+                width: 20,
+                height: 20
+              )
+          }
+        }.frame(
+          maxWidth: .infinity
+        )
+        
+      }
+      .padding()
+    }
+  }
+}
